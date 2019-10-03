5fcdde85cf21b9dfadb15a2e453bb4483b53f71d
nikumbgo <prtpgrv@yahoo.com>
2019-10-04 00:00:14 +0530

    git compare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { BodyComponent } from './home/body/body.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    BodyComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
